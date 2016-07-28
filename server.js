b2c479103742e2deac33a822ec1923a8026bda64
Robert Greenfield <robertgreenfield@Roberts-MacBook-Pro.local>
2016-07-28 15:27:55 -0400

    added q query to GET

--- server.js
+++ server.js
@@ -31,13 +31,13 @@ app.get('/todos', function(req, res) {
   {
     filteredTodos = _.where(filteredTodos, { completed: false});
     return res.json(filteredTodos);
-  } 
+  }
 
   //?q="" query set to string
   if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0)
   {
     filteredTodos = _.filter(filteredTodos, function(todo) {
-      return todo.description.indexOf(queryParams.q) > -1;
+      return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
     });
   }
 
